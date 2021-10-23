509f5b4e980100ba1ca63488f9e782a6db460d74
GrigoryCh <91157056+GrigoryCh@users.noreply.github.com>
2021-10-23 13:47:49 +0300

    Add files via upload

---
+++ #src/js/script.js
@@ -0,0 +1,266 @@
+
+"use strict";
+// Меню Бургер
+const iconMenu = document.querySelector('.icon-menu');
+const bodyMenu = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener (
+		"click", function (e) {
+	document.body.classList.toggle('_lock');
+	iconMenu.classList.toggle('_active');
+	bodyMenu.classList.toggle('_active');
+});
+}
+// Плавная прокрутка
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]'); /* Ищем menu__item где есть data-goto*/
+if (menuLinks.length > 0){/*Если есть проходим по их списку и по клику вызываем функцию onMenuLinkClick */
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick);
+	});
+	
+	function onMenuLinkClick (e) {
+		const menuLink = e.target; /* Выбираем целевой объект на который мы кликнули то есть наш пункт меню (ссылка)*/
+		/*Проверить заполнен ли data-goto атрибут и существует ли объект, на который он ссылается */
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+			//- document.querySelector('header').offsetHeight; должна быть высота шапки, но ее здесь нет
+			// так как main-block фулл скриновый				
+			//gotoBlock.getBoundingClientRect().top это Y-координата блока относительно окна браузера
+			//pageYOffset это количество прокрученных пикселей
+
+			// Закрывать меню Бургер при клике
+			// Если меню Бургер активно
+			if(iconMenu.classList.contains('_active')) {
+			// Удалить все добавленные ранее классы
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				bodyMenu.classList.remove('_active');
+			}
+
+						
+			window.scrollTo({ /*Прокрутить к верху блока, указанного в data-goto в html плавно*/
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault(); /*Чтобы отключить href для ссылки*/
+		}
+	}
+}
+
+function testWebP(callback) {
+var webP = new Image(); webP.onload = webP.onerror = function () { callback(webP.height == 2); }; webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+
+}
+
+testWebP(function (support) {
+if (support == true) { document.querySelector('body').classList.add('webp'); }else{ document.querySelector('body').classList.add('no-webp'); }
+
+});
+
+function _ibg(){
+let _ibg=document.querySelectorAll("._ibg"); for (var i = 0; i < _ibg.length; i++) { if(_ibg[i].querySelector('img')){ _ibg[i].style.backgroundImage = 'url('+_ibg[i].querySelector('img').getAttribute('src')+')'; } }
+
+}
+
+_ibg();
+
+//===Работа с формой=======================================================================
+
+// Cобытия focus и blur
+
+const mainForm = document.forms.main;
+const mainFormInput = mainForm.Name;
+
+const mainFormInputPlaceholder = mainFormInput.placeholder;
+
+mainFormInput.addEventListener("focus", function (e) {
+	mainFormInput.placeholder = "";
+});
+mainFormInput.addEventListener("blur", function (e) {
+	mainFormInput.placeholder = mainFormInputPlaceholder;
+});
+
+const mainFormEmail = mainForm.Email;
+const mainFormEmailPlaceholder = mainFormEmail.placeholder;
+
+mainFormEmail.addEventListener("focus", function (e) {
+	mainFormEmail.placeholder = "";
+});
+mainFormEmail.addEventListener("blur", function (e) {
+	mainFormEmail.placeholder = mainFormEmailPlaceholder;
+});
+
+const mainFormMessage = mainForm.Message;
+const mainFormMessagePlaceholder = mainFormMessage.placeholder;
+
+mainFormMessage.addEventListener("focus", function (e) {
+	mainFormMessage.placeholder = "";
+});
+mainFormMessage.addEventListener("blur", function (e) {
+	mainFormMessage.placeholder = mainFormMessagePlaceholder;
+});
+
+//====================================================================================================================
+// Динамический адаптив c Github
+// Dynamic Adapt v.1
+// HTML data-da="where(uniq class name),when(breakpoint),position(digi)"
+// e.x. data-da=".item,992,2"
+// Andrikanych Yevhen 2020
+// https://www.youtube.com/c/freelancerlifestyle
+
+"use strict";
+
+function DynamicAdapt(type) {
+   this.type = type;
+}
+
+DynamicAdapt.prototype.init = function () {
+   const _this = this;
+   // массив объектов
+   this.оbjects = [];
+   this.daClassname = "_dynamic_adapt_";
+   // массив DOM-элементов
+   this.nodes = document.querySelectorAll("[data-da]");
+
+   // наполнение оbjects объктами
+   for (let i = 0; i < this.nodes.length; i++) {
+      const node = this.nodes[i];
+      const data = node.dataset.da.trim();
+      const dataArray = data.split(",");
+      const оbject = {};
+      оbject.element = node;
+      оbject.parent = node.parentNode;
+      оbject.destination = document.querySelector(dataArray[0].trim());
+      оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+      оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+      оbject.index = this.indexInParent(оbject.parent, оbject.element);
+      this.оbjects.push(оbject);
+   }
+
+   this.arraySort(this.оbjects);
+
+   // массив уникальных медиа-запросов
+   this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
+      return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
+   }, this);
+   this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+      return Array.prototype.indexOf.call(self, item) === index;
+   });
+
+   // навешивание слушателя на медиа-запрос
+   // и вызов обработчика при первом запуске
+   for (let i = 0; i < this.mediaQueries.length; i++) {
+      const media = this.mediaQueries[i];
+      const mediaSplit = String.prototype.split.call(media, ',');
+      const matchMedia = window.matchMedia(mediaSplit[0]);
+      const mediaBreakpoint = mediaSplit[1];
+
+      // массив объектов с подходящим брейкпоинтом
+      const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
+         return item.breakpoint === mediaBreakpoint;
+      });
+      matchMedia.addListener(function () {
+         _this.mediaHandler(matchMedia, оbjectsFilter);
+      });
+      this.mediaHandler(matchMedia, оbjectsFilter);
+   }
+};
+
+DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+   if (matchMedia.matches) {
+      for (let i = 0; i < оbjects.length; i++) {
+         const оbject = оbjects[i];
+         оbject.index = this.indexInParent(оbject.parent, оbject.element);
+         this.moveTo(оbject.place, оbject.element, оbject.destination);
+      }
+   } else {
+      for (let i = 0; i < оbjects.length; i++) {
+         const оbject = оbjects[i];
+         if (оbject.element.classList.contains(this.daClassname)) {
+            this.moveBack(оbject.parent, оbject.element, оbject.index);
+         }
+      }
+   }
+};
+
+// Функция перемещения
+DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+   element.classList.add(this.daClassname);
+   if (place === 'last' || place >= destination.children.length) {
+      destination.insertAdjacentElement('beforeend', element);
+      return;
+   }
+   if (place === 'first') {
+      destination.insertAdjacentElement('afterbegin', element);
+      return;
+   }
+   destination.children[place].insertAdjacentElement('beforebegin', element);
+}
+
+// Функция возврата
+DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+   element.classList.remove(this.daClassname);
+   if (parent.children[index] !== undefined) {
+      parent.children[index].insertAdjacentElement('beforebegin', element);
+   } else {
+      parent.insertAdjacentElement('beforeend', element);
+   }
+}
+
+// Функция получения индекса внутри родителя
+DynamicAdapt.prototype.indexInParent = function (parent, element) {
+   const array = Array.prototype.slice.call(parent.children);
+   return Array.prototype.indexOf.call(array, element);
+};
+
+// Функция сортировки массива по breakpoint и place 
+// по возрастанию для this.type = min
+// по убыванию для this.type = max
+DynamicAdapt.prototype.arraySort = function (arr) {
+   if (this.type === "min") {
+      Array.prototype.sort.call(arr, function (a, b) {
+         if (a.breakpoint === b.breakpoint) {
+            if (a.place === b.place) {
+               return 0;
+            }
+
+            if (a.place === "first" || b.place === "last") {
+               return -1;
+            }
+
+            if (a.place === "last" || b.place === "first") {
+               return 1;
+            }
+
+            return a.place - b.place;
+         }
+
+         return a.breakpoint - b.breakpoint;
+      });
+   } else {
+      Array.prototype.sort.call(arr, function (a, b) {
+         if (a.breakpoint === b.breakpoint) {
+            if (a.place === b.place) {
+               return 0;
+            }
+
+            if (a.place === "first" || b.place === "last") {
+               return 1;
+            }
+
+            if (a.place === "last" || b.place === "first") {
+               return -1;
+            }
+
+            return b.place - a.place;
+         }
+
+         return b.breakpoint - a.breakpoint;
+      });
+      return;
+   }
+};
+
+const da = new DynamicAdapt("max");
+da.init();
